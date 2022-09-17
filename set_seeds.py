3fa4816cefb5cacd2306efac182db834a7cf8b97
MingzheHu <58562242+MingzheHu-Duke@users.noreply.github.com>
2022-09-17 14:44:23 -0400

    Create set_seeds.py

---
+++ set_seeds.py
@@ -0,0 +1,11 @@
+def set_seed(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+
+
+# set_seed(42)
